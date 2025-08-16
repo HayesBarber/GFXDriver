2007b4418a05bb25c167a6a7069947288a7693ed
Hayes Barber <barberhayes@gmail.com>
2025-08-16 10:33:24 -0400

    change version

--- src/GFXDriver.cpp
+++ src/GFXDriver.cpp
@@ -5,17 +5,14 @@ GFXDriver::GFXDriver() : _onTouch(nullptr) {
                            0 /* MOSI */, GFX_NOT_DEFINED /* MISO */);
 
   _rgbpanel = new Arduino_ESP32RGBPanel(
-      2 /* DE */, 42 /* VSYNC */, 3 /* HSYNC */, 45 /* PCLK */, 11 /* R0 */,
-      15 /* R1 */, 12 /* R2 */, 16 /* R3 */, 21 /* R4 */, 39 /* G0/P22 */,
+      2 /* DE */, 42 /* VSYNC */, 3 /* HSYNC */, 45 /* PCLK */, 4 /* R0 */,
+      41 /* R1 */, 5 /* R2 */, 40 /* R3 */, 6 /* R4 */, 39 /* G0/P22 */,
       7 /* G1/P23 */, 47 /* G2/P24 */, 8 /* G3/P25 */, 48 /* G4/P26 */,
-      9 /* G5 */, 4 /* B0 */, 41 /* B1 */, 5 /* B2 */, 40 /* B3 */, 6 /* B4 */,
-      1 /* hsync_polarity */, 10 /* hsync_front_porch */,
+      9 /* G5 */, 11 /* B0 */, 15 /* B1 */, 12 /* B2 */, 16 /* B3 */,
+      21 /* B4 */, 1 /* hsync_polarity */, 10 /* hsync_front_porch */,
       8 /* hsync_pulse_width */, 50 /* hsync_back_porch */,
       1 /* vsync_polarity */, 10 /* vsync_front_porch */,
-      8 /* vsync_pulse_width */, 20 /* vsync_back_porch */,
-      0 /* pclk_active_neg */, 12000000 /* prefer_speed */,
-      false /* useBigEndian */, 0 /* de_idle_high */, 0 /* pclk_idle_high */,
-      0 /* bounce_buffer_size_px */);
+      8 /* vsync_pulse_width */, 20 /* vsync_back_porch */);
 
   _gfx = new Arduino_RGB_Display(
       DISPLAY_WIDTH /* width */, DISPLAY_HEIGHT /* height */, _rgbpanel,
