1893ea6ececcd4a2f628dd96221ad85b59c008f8
Hayes Barber <barberhayes@gmail.com>
2025-08-16 13:14:09 -0400

    fill rect

--- src/GFXDriver.cpp
+++ src/GFXDriver.cpp
@@ -136,9 +136,13 @@ void GFXDriver::drawPowerSymbol(Third third) {
   static int16_t radius = 20;
   static int16_t lineThickness = 4;
   static int16_t circleThickness = radius - lineThickness;
+  static int16_t padding = 4;
 
   ThirdCenter center = THIRD_CENTERS[third];
 
   _gfx->fillCircle(center.x, center.y, radius, WHITE);
   _gfx->fillCircle(center.x, center.y, circleThickness, BLACK);
+
+  _gfx->fillRect(center.x - ((lineThickness + padding) / 2), center.y,
+                 lineThickness + padding, radius + padding, BLACK);
 }
