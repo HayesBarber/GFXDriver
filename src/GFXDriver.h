aecf9a58034038ed56b154409db690f0e7ce18fb
Hayes Barber <barberhayes@gmail.com>
2025-08-15 21:04:24 -0400

    add loop method

--- src/GFXDriver.h
+++ src/GFXDriver.h
@@ -8,17 +8,21 @@
 #define I2C_SCL_PIN 18
 #define DISPLAY_WIDTH 480
 #define DISPLAY_HEIGHT 480
-#define DEBOUNCE_DELAY 300
+#define DEBOUNCE_DELAY_MS 300
 
 class GFXDriver {
 public:
   GFXDriver();
   void init(void (*onTouch)());
   void writeText(String text);
+  void loop();
 
 private:
   void (*_onTouch)();
   Arduino_DataBus *_bus;
   Arduino_ESP32RGBPanel *_rgbpanel;
   Arduino_RGB_Display *_gfx;
+
+  void i2cRead(uint16_t addr, uint8_t reg_addr, uint8_t *reg_data,
+               uint32_t length);
 };
